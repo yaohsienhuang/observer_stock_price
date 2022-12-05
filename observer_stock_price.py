17454edf8e230489ed37316c47487c7500e5dfa8
yaohsienhuang <hsien.huang@outlook.com>
2022-12-05 11:44:45 +0800

    feat:addObserver(profit_obser)

--- observer_stock_price.py
+++ observer_stock_price.py
@@ -40,25 +40,54 @@ class priceObser(Observer):
     def update(self, stockPrice):
         for name,now_price in stockPrice.price.items():
             if name in self.obser_dict.keys():
-                print(f'{name} 在觀察清單中')
+                print(f'[價格提醒] {name} 在條件單中')
                 
                 alert=False
                 for action,detail in self.obser_dict[name].items():
                     prices,positions=[a['price'] for a in detail],[a['position'] for a in detail]
                     if now_price in prices:
                         position=positions[prices.index(now_price)]
-                        print(f'[{action}] {name} {action} {position} 張')
+                        print(f'[價格提醒] {name} 觸發條件({action}:{now_price}元,{position}張) 建議 {action} {position} 張')
                         alert=True
                         
                 if alert==False:
-                    print('未觸發任何條件，不做任何動作') 
+                    print('[價格提醒] 未觸發任何條件，不做任何動作') 
             else:
-                print(f'{name} 不在觀察清單中')
+                print(f'[價格提醒] {name} 不在條件單中')
+
+class profitObser(Observer):
+    def __init__(self,obser_dict):
+        self.obser_dict=obser_dict
+        self.avg_dict=self.avg_price()
+        
+    def update(self, stockPrice):
+        for name,now_price in stockPrice.price.items():
+            if name in self.obser_dict.keys():
+                price,position=self.avg_dict[name]['price'],self.avg_dict[name]['position']
+                profit=(now_price-price)*position*1000
+                profit_ratio=round((now_price-price)/price,1)
+                print(f'[部位提醒] {name} 持有 {position} 張部位，平均價格 {price}，平均獲利 {profit_ratio} %，共獲利 {profit} 元。')
+            else:
+                print(f'[部位提醒] {name} 未持有部位')
+                    
+        
+    def avg_price(self):
+        avg_dict=dict()
+        for name,detail in self.obser_dict.items():
+            sum_product=sum([a['price']*a['position'] for a in detail])
+            sum_position=sum([a['position'] for a in detail])
+            avg_price=round(sum_product/sum_position,1)
+            avg_dict[name]={'price':avg_price,'position':sum_position}
+        return avg_dict
+            
+        
 
 if __name__=='__main__':
     # 實體化 stockPrice 命名為 notifier
     notifier = stockPrice()
-    obser_dict={
+    
+    # 實體化 監聽者:priceObser(監聽條件)
+    conditional_dict={
         'TSMC':{
             'buy':[
                 {'price':362,'position':1},
@@ -72,14 +101,25 @@ if __name__=='__main__':
             ],
         }
     }
-    # 實體化 監聽者:priceObser(監聽條件)
-    price_obser = priceObser(obser_dict)
+    conditional_obser = priceObser(conditional_dict)
+    
+    # 實體化 監聽者:profitObser(監聽條件)
+    poistion_dict={
+        'TSMC':[
+            {'price':245,'position':1},
+            {'price':276,'position':2},
+            {'price':324,'position':1}
+        ]
+    }
+    profit_obser = profitObser(poistion_dict)
     
     # 將 監聽者 加入 notifier
-    notifier.addObserver(price_obser)
+    notifier.addObserver(profit_obser)
+    notifier.addObserver(conditional_obser)
+    
 
     # 將當前價格 帶入 price.setter -> priceObser會針對當前價格做出反應動作
-    notifier.price={'TSMC':600}
+    notifier.price={'TSMC':495}
 
 
 
